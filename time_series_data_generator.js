fad415dfffa3a09a0721d6c558fb0bf689ca1941
Akiyah <akiya.mizukoshi@gmail.com>
2019-09-17 16:36:56 +0900

    Stacked Area Chart

--- time_series_data_generator.js
+++ time_series_data_generator.js
@@ -1,23 +1,37 @@
 class TimeSeriesDataGenerator {
-  static generate(start, end, freq, option) {
-    var startDate = new Date(start);
-    var endDate = new Date(end);
-    var d = new Date(start);
-    var i = 0;
-    var data = [];
-    while (d <= endDate) {
-      var v = this.generateByDayCount(i, option);
-      data.push([d.getTime(), v]);
-      i = i + freq;
-      d.setDate(d.getDate() + freq);
-    }
-    return data;
+  constructor(start, end, freq) {
+    this.start = start;
+    this.end = end;
+    this.freq = freq;
   }
 
-  static generateByDayCount(i, option) {
-    var value = option['base'] + option['random']*(Math.random()*2-1);
-    var waves = option['waves'];
-    waves.forEach(function(wave) {
+  generateDates() {
+    const self = this;
+    const endDate = new Date(self.end);
+    const d = new Date(self.start);
+    const dates = [];
+    while (d <= endDate) {
+      dates.push(d.getTime());
+      d.setDate(d.getDate() + self.freq);
+    }
+    return dates;
+  }
+
+  generateValues(option) {
+    const self = this;
+    const dates = this.generateDates();
+    const values = [];
+    dates.forEach((_value, i) => {
+      values.push(self.generateValueByDayCount(i * self.freq, option));
+    });
+    return values;
+  }
+
+  generateValueByDayCount(i, option) {
+    const self = this;
+    let value = option['base'] + option['random']*(Math.random()*2-1);
+    const waves = option['waves'];
+    waves.forEach(wave => {
       value += Math.sin(i/wave[0]*Math.PI*2)*wave[1];
     });
     return Math.round(value);
